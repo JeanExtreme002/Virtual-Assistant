1444af964b21c6d043b1b8ccb23f34999c5fd81a
JeanExtreme002 <jeangamerextreme@gmail.com>
2021-07-09 02:35:31 -0300

    created init_mixer() function

--- src/util/system/sound.py
+++ src/util/system/sound.py
@@ -1,7 +1,14 @@
 from pygame import mixer
 
-mixer.init(buffer = 64)
+__all__ = ("init_mixer", "play_sound")
+
+is_mixer_initialized = False
+
+def init_mixer(buffer = 64):
+    mixer.init(buffer = buffer)
+    is_mixer_initialized = True
 
 def play_sound(filename_or_fp):
+    if not is_mixer_initialized: init_mixer()
     mixer.music.load(filename_or_fp)
     mixer.music.play()
